8072caad0ec30fe1430d9a2031c9d683cc4f3583
vladd111-source <help.implant@gmail.com>
2025-06-07 12:41:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const supabase = createClient(
 
 // Telegram init
 const token = process.env.BOT_TOKEN;
-const bot = new TelegramBot(token);
+const bot = new TelegramBot(token, { polling: true });
 
 // 🔐 Админ ID
 const ADMIN_ID = 5625134095;
